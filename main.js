353c48b2e0cf9052f7a7f94173bea5be5687f402
MatteoMancuso-92 <Matteotrieste@outlook.com>
2023-05-03 19:05:25 +0200

    js

--- main.js
+++ main.js
@@ -38,7 +38,8 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=20" 
+            //TO DO: REMOVE LINK 
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -55,3 +56,39 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const elePostContainer = document.querySelector(".posts-list");
+
+posts.forEach((element) => {
+    elePostContainer.innerHTML += 
+    `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${element.author.image} alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${element.content}</div>
+        <div class="post__image">
+            <img src=${element.media} alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+});
